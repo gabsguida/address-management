fcc66693ecc659d3204b022a0643a7e9d561ea34
Gabriela Guida <gabrielaguida7@gmail.com>
2022-05-22 23:08:08 -0300

    add: creating components for icons and using it in header and sidebar components

---
+++ src/components/UiButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :disabled="disabled"
+    @click="$emit('click', $event)"
+    @focus="$emit('focus', $event)"
+    @blur="$emit('blur', $event)"
+    :type="type"
+    :class="{
+      'pointer-events-none opacity-50 cursor-not-allowed': disabled,
+      relative: to != null,
+    }"
+    class="transition rounded-lg focus:outline-none active:outline-none inline-flex items-center whitespace-nowrap focus:bg-opacity-10 hover:bg-opacity-60"
+  >
+    <router-link
+      v-if="to != null"
+      :to="to"
+      class="absolute inset-0 z-10 w-full h-full"
+    ></router-link>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "UiButton",
+  emits: ["click", "focus", "blur"],
+  props: {
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: false,
+      default: "button",
+    },
+    to: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
